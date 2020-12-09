3b73d5c75136a4c1e26669053c6170f29263b90a
Zhixing Wang <isofew@gmail.com>
2020-12-09 15:09:54 +0800

    add back modified data.py and spark.py

---
+++ sodata/scripts/spark.py
@@ -0,0 +1,12 @@
+from pyspark.sql import SparkSession
+
+def get_spark(appName, mem, cores):
+    return (
+        SparkSession
+        .builder
+        .config('spark.driver.memory', f'{mem}g')
+        .config('spark.sql.session.timeZone', 'UTC')
+        .appName('count_per_user_bin')
+        .master(f'local[{cores}]')
+        .getOrCreate()
+    )
